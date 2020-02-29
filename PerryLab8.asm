bd8625bc426546df2eec2cfff60b585d3912bff7
neillperry <chainsaw.bucktooth@gmail.com>
2020-02-29 08:41:01 -0500

    cannibalize code from Lab 7 into Lab 8, but that's showbusiness, baby

--- PerryLab8.asm
+++ PerryLab8.asm
@@ -1,5 +1,5 @@
 ########## PerryLab8.asm ##########
-
+#
 #         Title:  Lab 8 - Extend minmax                #
 #         Author: Neill Perry                          #
 #         Date: March 3, 2020                          #
@@ -16,28 +16,85 @@
 #             Addition Variables:                      #
 #                                                      #
 #         Formulas:                                    #
-
+#
 ####################################
-
-
-
-############ minMax.asm on p. 65-66 ##############
-#                                                #
-# Illustrates Base Addressing                    #
-#                                                #
-# Enter an array of numbers and find the         #
-# minimum and maximum of the array               #
-#                                                #
-# t1 = count, t2 = min, t3 = max                 #
-# t0 = counter, t4 = array element               #
-# t5 = address of array element, t6 = temp       #
-#                                                #
-##################################################
 
        .text
       .globl main
 
 main:
+      #############################
+      ##                         ##
+      ##  Ask User to Set Array  ##
+      ##        Size n           ##
+      ##                         ##
+      #############################
+
+      # A. Get integer n from User
+      la $a0, arraysize     # load address of string output into a0
+      li $v0,4              # load instruction number to display a string into v0
+      syscall               # v0 = 4, indicates display a string
+
+      #Get the integer from User
+      li $v0, 5             # load instruction to read an integer from keyboard
+      syscall               # system call to read integer and store in $f0
+
+      # Store the result from $v0 to $t0
+      move $t0, $v0         # t1 will be the counter for the while loop below
+
+      # STEP 1 - check to see if user input is greater than 15
+      blt $t0, 15 ELSE        # IF input < 15 then move on to getting integers
+        li $t0, 15            # IF input > 15, reset it to 15
+        j ENDIF               # done with IF so move to ENDIF label
+
+      # STEP 2 - terminate IF statement, augmenting the counter, restart WHILE loop
+      ENDIF:
+        # Insert next line
+        la $a0, endl          # load address of new line into a0
+        li $v0,4              # load instruction number to display a string into v0
+        syscall               # v0 = 4, indicates display a string
+
+      #############################
+      ##                         ##
+      ##   Get n User Integers   ##
+      ##                         ##
+      #############################
+
+      # B. Initialize array pointer
+      li $t1, 0             # initialize the counter (t1) to 0
+      li $t2, 0             # initialize the pointer (t2) to 0
+
+      WHILE:
+        bgt, $t1, $t0, ENDWHILE
+        # A. Prompt User to Enter an integer
+        la $a0, prompt        # load address of prompt into a0
+        li $v0,4              # load instruction number to display a string into v0
+        syscall               # v0 = 4, indicates display a string
+
+        #Get the integer from User
+        li $v0, 5             # load instruction to read an integer from keyboard
+        syscall               # system call to read integer and store in $f0
+
+        # Store the result from $v0 to array
+        sw $v0, array($t2)
+
+        add $t1, $t1, 1          # increment loop counter by 1
+        add $t2, $t2, 4          # increment the array pointer by 4 (bytes)
+
+        j WHILE                  # start the loop again
+
+      ENDWHILE: la $a0, endl     # load beginning address of display message into a0 register
+          li $v0,4               # load call code to print a string
+          syscall                # system call to start a new line
+
+      # Now that we have our integer array, let's find min and max!!
+
+      #############################
+      ##                         ##
+      ##     Find Min / Max      ##
+      ##                         ##
+      #############################
+
       la $t0,array        # t0 = address of array
       lw $t1,count        # t1 = count, exit loop when it goes to 0
       lw $t2,($t0)        # t2 = min = a[0] (initialization)
@@ -46,18 +103,36 @@ main:
       add $t1,$t1,-1      # decrement counter to keep in step with array
 
 loop:  lw $t4,($t0)       # t4 = next element in array
-      bge $t4,$t2,notMin  # if array element is >= min goto notMin
-      move $t2,$t4        # min = a[i]
+
+      # Display Array Element (t4)
+      move $a0, $t4          # move counter from t3 --> a0 register
+      li $v0,1               # load call code to print the integer
+      syscall                # system call to print the integer
+
+      # Add a space after Array Element
+      la $a0, space          # load beginning address of display message into a0 register
+      li $v0,4               # load call code to print a string
+      syscall                # system call to start a new line
+
+      # IF statement
+      bge $t4,$t2,notMin    # if array element is >= min goto notMin
+      move $t2,$t4          # min = a[i]
       j notMax
 
-notMin: ble $t4,$t3,notMax # if array element is <= max goto notMax
-      move $t3,$t4        # max = a[i]
+notMin: ble $t4,$t3,notMax  # if array element is <= max goto notMax
+      move $t3,$t4          # max = a[i]
 
 notMax: add $t1,$t1,-1      # t1 -- -> counter --
-      add $t0,$t0,4       # increment counter to point to next word
+      add $t0,$t0,4         # increment counter to point to next word
       bnez $t1,loop
-      
-      la $a0,p1           # Display "The minimum number is "
+
+      #############################
+      ##                         ##
+      ##     Display Results     ##
+      ##                         ##
+      #############################
+
+      la $a0,min          # Display "The minimum number is "
       li $v0,4            # a0 = address of message
       syscall             # v0 = 4 which indicates display a string
 
@@ -65,7 +140,7 @@ notMax: add $t1,$t1,-1      # t1 -- -> counter --
       li $v0,1
       syscall
 
-      la $a0,p2           # Display "The maximum number is "
+      la $a0,max          # Display "The maximum number is "
       li $v0,4            # a0 = address of message
       syscall             # v0 = 4 which indicates display a string
 
@@ -73,23 +148,19 @@ notMax: add $t1,$t1,-1      # t1 -- -> counter --
       li $v0,1
       syscall
 
-      la $a0,crlf         # Display "cr/lf"
-      li $v0,4            # a0 = address of message
-      syscall             # v0 = 4 which indicates display a string
-
       li $v0,10           # End Of Program
       syscall
 
 .data
+      arraysize:   .asciiz  "How big is your array?"    # Prompt user for array size
+      prompt:      .asciiz  "Enter an integer: "        # Prompt for integers
+      max:         .asciiz  "\n"The maximum number is " # Line showing average
+      min:         .asciiz  "\n"The minimum number is " # Line showing average
+      space:       .asciiz  "  "                        # Space to insert b/w numbers
+      endl:        .asciiz  "\n"                        # Start new line
 
-p1:     .asciiz "The minimum number is "
-p2:     .asciiz "\nThe maximum number is "
-crlf:   .asciiz "\n"
-array: .word 3, 4, 2, 6, 12, 7, 18, 26, 2, 14, 19, 7, 8, 12, 13
 
-count: .word 15
-
-
+      array:       .word 80                            # every integer needs 4 bytes
 
 
 ################ Output #################
